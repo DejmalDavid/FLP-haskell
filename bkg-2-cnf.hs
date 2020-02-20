7cd510f61670e095814856125e3b6dac0fef0a2d
David Dejmal <dejmal.d@gmail.com>
2020-02-20 23:59:27 +0100

    git ignore

--- bkg-2-cnf.hs
+++ bkg-2-cnf.hs
@@ -2,17 +2,17 @@
 David Dejmal, xdejma00 VUT FIT FLP 2020
 --}
 import System.Environment   
+import Data.Char  
   
 main = do  
-   args <- getArgs     
+    args <- getArgs  
+    contents <- getContents   
 -- parsovani argumentu ve tvaru bkg-2cnf volby [vstup]  --
-   case args of
-        ["-i"] -> putStrLn "param i"  
-        ["-1"] -> putStrLn "param 1"  
+    case args of
+        ["-i"] -> putStrLn "param i" >> putStrLn "param i"
+        ["-1"] -> putStrLn "param 1" >> putStr (map toUpper contents)  
         ["-2"] -> putStrLn "param 2"  
         ["-i",_] -> putStrLn "param i + file"  
         ["-1",_] -> putStrLn "param 1+ file"  
         ["-2",_] -> putStrLn "param 2+ file"  
         otherwise -> putStrLn "Usage: bkg-2cnf volby [vstup]"  
-        
-        
