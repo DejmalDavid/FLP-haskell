d55c6afca0566c990028cca4fcce3ef3999e8a07
David Dejmal <dejmal.d@gmail.com>
2020-02-21 16:55:02 +0100

    parsovani a nacitani souboru je sexy

--- bkg-2-cnf.hs
+++ bkg-2-cnf.hs
@@ -3,16 +3,32 @@ David Dejmal, xdejma00 VUT FIT FLP 2020
 --}
 import System.Environment   
 import Data.Char  
-  
+
+-- main funkce programu --
+main :: IO ()
 main = do  
     args <- getArgs  
-    contents <- getContents   
--- parsovani argumentu ve tvaru bkg-2cnf volby [vstup]  --
-    case args of
-        ["-i"] -> putStrLn "param i" >> putStrLn "param i"
-        ["-1"] -> putStrLn "param 1" >> putStr (map toUpper contents)  
-        ["-2"] -> putStrLn "param 2"  
-        ["-i",_] -> putStrLn "param i + file"  
-        ["-1",_] -> putStrLn "param 1+ file"  
-        ["-2",_] -> putStrLn "param 2+ file"  
-        otherwise -> putStrLn "Usage: bkg-2cnf volby [vstup]"  
+    
+    let parametry = parsovaniArgumentu(args) 
+    putStrLn ( show (parametry))
+    
+    let obsah = nacti(parametry)
+    
+    str <- obsah
+    putStrLn(str)
+    
+-- parsovani argumentu ve tvaru bkg-2cnf volby [vstup]  --    
+-- parsovaniArgumentu :: [[Char]] -> (Char, [Char])
+parsovaniArgumentu ["-i"] = ('i',"")
+parsovaniArgumentu ["-1"] = ('1',"")
+parsovaniArgumentu ["-2"] = ('2',"")
+parsovaniArgumentu ["-i",file] = ('i',file)
+parsovaniArgumentu ["-1",file] = ('1',file)
+parsovaniArgumentu ["-2",file] = ('2',file)
+parsovaniArgumentu _ = ('E',"Chyba")
+    
+
+nacti (_,"") = getContents
+nacti (_,file)  = readFile file
+        
+    
