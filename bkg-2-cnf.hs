6dde159c2463ba727d250792c73839ed284e7772
David Dejmal <dejmal.d@gmail.com>
2020-03-25 21:41:07 +0100

    bomby

--- bkg-2-cnf.hs
+++ bkg-2-cnf.hs
@@ -13,15 +13,12 @@ import Data.List -- \\
 import qualified Data.Set as Set
 import Debug.Trace
 
-data Pravidlo = Pravidlo { neterminal :: String
-        , produkt :: String
-        } deriving (Eq, Show)
-
-data Gramatika = Gramatika { neterminaly :: [String]
-        , terminaly :: [String]
-        , pocatecniNeterminal :: String
-        , pravidla :: [Pravidlo]
-        } deriving (Show)
+-- datova struktura pro gramatiku
+data Gramatika = Gramatika { neterminaly :: [String] -- neterminaly
+        , terminaly :: [String] --terminaly
+        , pocatecniNeterminal :: String     --pocatecni neterminal
+        , pravidla :: [(String, [String])] -- seznam pravidel
+        } deriving (Show) --pro testovaci ucely 
 
 
 -- main funkce programu --
@@ -29,66 +26,194 @@ main :: IO ()
 main = do  
     args <- getArgs  
     
-    let parametry = parsovaniArgumentu(args) 
+    let parametry = parsovaniArgumentu(args) --cteni argumentu
     putStrLn ( show (parametry))    -- TODO 
     
-    let obsah = nacti(parametry)
+    let obsah = nacti(parametry) --nacteni obsahu podle prepinacu
     str <- obsah
     
-    doStufs parametry str
+    doStufs parametry str   --hlavni funkce programu
 
-
+-- nacteni do vnitrni struktury a vypis
 doStufs ('i',_) content = do
-    let gramatika = parsujVstup(lines content)
+    let gramatika = parsujVstup(lines content)  --nacteni
     --putStrLn(show gramatika)
+    --semanticka kontrola gramatiky
     let a = testPocatecni gramatika
     let b = testRulse gramatika
 
     if (a && b == True)
-    then printGramatika gramatika
+    then printGramatika gramatika   --vypis
     else error "Semanticka chyba gramatiky!!"
 
 
+-- nacteni, odstaneni jednoduchych pravidel a vypis
+doStufs ('1',_) content = do 
+    --putStrLn("volam jsem 1") 
+    let gramatika = parsujVstup(lines content)--nacteni
+    putStrLn(show gramatika)
+        --semanticka kontrola gramatiky
+    let a = testPocatecni gramatika
+    let b = testRulse gramatika
 
-doStufs ('1',_) content = putStrLn("volam jsem 1") >> fukce1 (content)  -- TODO
-doStufs ('2',_) content = putStrLn("volam jsem 2") >> fukce2 (content)  -- TODO
+    if (a && b == False)
+    then error "Semanticka chyba gramatiky!!"
+    else do 
+    let gramtika_bez = getGramatikaBez gramatika --odstraneni jednoduchych pravidel
+    --putStrLn(show gramtika_bez)
+    printGramatika gramtika_bez --vypis
+
+
+doStufs ('2',_) content = do    
+    putStrLn("volam jsem 2") 
+    let gramatika = parsujVstup(lines content)--nacteni
+    --putStrLn(show gramatika)
+    --semanticka kontrola gramatiky
+    let a = testPocatecni gramatika
+    let b = testRulse gramatika
+
+    if (a && b == False)
+    then error "Semanticka chyba gramatiky!!"
+    else do 
+    let gramtika_bez = getGramatikaBez gramatika --odstraneni jednoduchych pravidel
+    putStrLn(show gramtika_bez)
+    let gramtika_CNF = getGramatikaCNF gramtika_bez --odstraneni jednoduchych pravidel
+    printGramatika gramtika_CNF --vypis
+
 doStufs _ content = error "Špatně uvedene parametry."
 
+
+--getGramatikaCNF gramatika = gramatika
+-----------------------START COPY PASTE ------------
+
+{-|
+    K jednomu pravidlu gramatiky vrátí seznam pravidel v chomského normální
+    formě.
+-}
+split_rule_to_cnf::Gramatika->(String, [String])->[(String, [String])]
+--pravidla A->a se vrací neupravená, jednoduché pravidlo způsobí chybu.
+split_rule_to_cnf g r@(_, x:[])
+    |(x`elem`(terminaly g))=[r]
+    |otherwise=error "Simple rule."
+--zpracování pravidla jehož pravá část se skládá ze dvou částí
+split_rule_to_cnf g r@(a, b:c:[])
+    |(b`elem`(terminaly g))&&(c`elem`(terminaly g))=[(a, [b++"'",c++"'"]),(b++"'",[b]),(c++"'",[c])]
+    |(b`elem`(terminaly g))=[(a, [(b++"'"),c]),(b++"'",[b])]
+    |(c`elem`(terminaly g))=[(a, [b,(c++"'")]),(c++"'",[c])]
+    |otherwise=[r]
+--zpracování pravidla jehož pravá část se skládá z více jak dvou částí
+split_rule_to_cnf g (a, x:xs)
+    --pravá strana začíná terminálen
+    |(x`elem`(terminaly g))=
+        (a, [(x++"\'"),nn]):(x++"\'", [x]):(split_rule_to_cnf g (nn, xs))
+    --pravá strana začíná neterminálen
+    |otherwise=
+        (a, [x, nn]):(split_rule_to_cnf g (nn, xs))
+    where nn="<"++(foldr1 (++) xs)++">"
+
+{-|
+    Získá neterminály z levých stran pravidel.
+-}
+get_nonterminals_from_rules::[(String, [String])]->[String]
+get_nonterminals_from_rules l=foldl step [] l
+    where step acc (n, _)=if n`elem`acc then acc else n:acc
+
+{-|
+    Převede gramatiku do chomského normální formy podle algoritmu 4.7 ze
+    studijní opory předmětu TIN.
+-}
+getGramatikaCNF::Gramatika->Gramatika
+getGramatikaCNF g=
+    --pravidla nové gramatiky
+    let rs=(foldl step [] (pravidla g))
+    in
+    Gramatika (terminaly g) ((neterminaly g)`union`(get_nonterminals_from_rules rs)) (pocatecniNeterminal g) rs
+    where 
+        step::[(String, [String])]->(String, [String])->[(String, [String])]
+        step acc item=acc`union`(split_rule_to_cnf g item)
+
+-----------------------END COPY PASTE ------------
+
+-- tvorba nove gramatiky bez jednoduchych pravidel
+getGramatikaBez:: Gramatika -> Gramatika
+getGramatikaBez gramatika = Gramatika (terminaly gramatika) --stejne
+                                      (neterminaly gramatika) --stejne
+                                      (pocatecniNeterminal gramatika) --stejne
+                                      (foldl (transform (neterminaly gramatika) (vytvorMnoziny gramatika)) [] (pravidla gramatika)) --nove vytvorene
+                                      where 
+                                        vytvorMnoziny gramatika = map (\x->(x, vytvorMnozinu gramatika [x])) (neterminaly gramatika) -- volani nad vsemi
+                                        
+                                        
+-- jadro algoritmu 4.5, vypocet mnoziny N 
+vytvorMnozinu::Gramatika->[String]->[String]
+vytvorMnozinu gramatika n0 = if n0 /= nx
+                             then vytvorMnozinu gramatika (nx) -- iteruje
+                             else n0    --konec
+                             where nx = union n0 (foldl (krok45 gramatika n0) [] (pravidla gramatika)) --sjednotit mnoziny a iterovat
+
+-- prida prvekt do mnoziny N     
+krok45::Gramatika->[String]->[String]->(String, [String])->[String]
+krok45 gramatika n0 senznam (nonterm, produkt:[]) = if (elem produkt (neterminaly gramatika)) && (elem nonterm n0)
+                                                    then (produkt:senznam)       
+                                                    else senznam --konec
+
+--transformace pravidel, tvorba novych 
+transform nonterms nMnoziny seznam (nonterm, produkt)=
+            if ((elem (head produkt) nonterms) && ((tail produkt) == [])) == False
+            then seznam ++ (map (\x->(x, produkt)) (najdiMnoziny nMnoziny nonterm)) --iterace
+            else seznam --konec
+
+--vypis
 printGramatika:: Gramatika -> IO ()
 printGramatika gramatika = do putStrLn (spoj (neterminaly gramatika))
                               putStrLn (spoj (terminaly gramatika))
                               putStrLn (pocatecniNeterminal gramatika)
-                              putStrLn (spoj' (pravidla gramatika))
+                              putStr (foldr1 (++) (map spojPravidla (pravidla gramatika)))
 
+--priprava na vypis
 spoj :: [String] -> String
 spoj [x]    = x
 spoj (x:xs) = x ++ [','] ++ (spoj xs) 
 
-spoj' :: [Pravidlo] -> String
-spoj' [x]    = toStr x
-spoj' (x:xs) = toStr x ++ ['\n'] ++ (spoj' xs)
+--hleda mnoziny a vytvori z nich seznam
+najdiMnoziny::[(String,[String])]->String->[String]
+najdiMnoziny [] _ = []
+najdiMnoziny ((nonterm,produkt):xs) neterminal = if elem neterminal produkt
+                                                 then nonterm:(najdiMnoziny xs neterminal)
+                                                 else najdiMnoziny xs neterminal
+--priprava na vypis pravidel
+spojPravidla :: (String,[String]) -> String
+spojPravidla (nonterm,term) = nonterm++'-':'>':(foldr1 (++) term)++['\n']
 
-toStr :: Pravidlo -> String
-toStr x = neterminal x ++ ['-']++ ['>'] ++ produkt x
-
-
-
+--sematicka kontrola
 testPocatecni gramatika =  elem (pocatecniNeterminal gramatika) (neterminaly gramatika)
+
+--sematicka kontrola pravidel
 testRulse  gramatika = testRule gramatika (pravidla gramatika)
 
-testRule:: Gramatika -> [Pravidlo] -> Bool
+--kontrola jednotliveho pravidla
+testRule:: Gramatika -> [(String, [String])] -> Bool
 testRule gramatika [] = True
-testRule gramatika (x:xs) = if (elem (neterminal x) (neterminaly gramatika) && (testRuleRight gramatika (produkt x)) )
+testRule gramatika ((non,term):xs)  = if (elem non (neterminaly gramatika) && (testRuleRight gramatika term)  )
                             then testRule gramatika xs
                             else False
                             
-testRuleRight:: Gramatika -> String -> Bool    
+--test prave strany pravidla                            
+testRuleRight:: Gramatika -> [String] -> Bool    
 testRuleRight gramatika [] = True                   
-testRuleRight gramatika (x:xs) = if (elem  [x] (neterminaly gramatika) || elem  [x] (terminaly gramatika) )
-                            then testRuleRight gramatika xs
-                            else False
-                            
+testRuleRight gramatika (x:xs) = if (checkTerms gramatika x)  
+                                 then testRuleRight gramatika xs
+                                 else False
 
+checkTerms:: Gramatika -> String -> Bool 
+checkTerms gramatika [] = True
+checkTerms gramatika (char:xs) = if (elem [char] (terminaly gramatika) || elem [char] (neterminaly gramatika) )
+                                 then checkTerms gramatika xs
+                                 else False
+                                 
+
+--elem [x] (terminaly gramatika) 
+--elem [x] (terminaly gramatika)
 -- parsovani argumentu ve tvaru bkg-2cnf volby [vstup]    
 parsovaniArgumentu :: [String] -> (Char, [Char])
 parsovaniArgumentu ["-i"] = ('i',"")
@@ -115,43 +240,44 @@ parsujVstup obsah = if ( length obsah < 4 )
 
 parsujNeterminaly  ( radek1 : _ ) =  map countOneNeterminal (splitOn "," radek1)
 parsujTerminaly (_: radek2 : _) = map countOneTerminal (splitOn "," radek2)
-
---TODO kontrola ze pocatecni je v neterminalech
 parsujPocatecni (_: _ : radek3 : _) = if (length radek3 == 1)
                                         then radek3
                                         else  error "Počáteční neterminal musi být pouze jeden!!"
-
---TODO kontrola zda byl nereminal(i produkt???) definovany
-
 parsujPravidla (_: _ : _ : radek4) = parsujPravidlo radek4
 
-parsujPravidlo seznam =  map (\t->createPravidlo t) seznam
+--parsovani pravidla je slozitejsi
+parsujPravidlo seznam = map (\t->createPravidlo t) seznam
+
+--jednotlive pravidlo
 createPravidlo t
     | length(rez)/=2 = error "Špatně zapsané pravidlo!!!"
-    | otherwise = Pravidlo { neterminal=from, produkt=over}
+    | otherwise = (non,[term])
     where   rez = splitOn "->" t
-            from = rez!!0
-            over = rez!!1
+            non = rez!!0
+            term = rez!!1
 
-
+--pomocna funkce
 isStringUpper:: [Char] -> Bool
 isStringUpper [] = True
 isStringUpper (x:xs) = if (isUpper x)
                         then isStringUpper xs
                         else False
-
+                        
+--pomocna funkce
 isStringLower:: [Char] -> Bool
 isStringLower [] = True
 isStringLower (x:xs) = if (isLower x)
                         then isStringLower xs
                         else False
-
+                        
+--pomocna funkce
 countOneNeterminal:: [Char] -> [Char]
 countOneNeterminal [] = error "Špatně uvedene Neterminaly! Čarka na konci řádku?"
 countOneNeterminal x = if (isStringUpper x && (length x == 1))
              then x
              else error "Špatně uvedene Neterminaly! Neterminal musi obsahovat pouze velke znaky!"
-
+             
+--pomocna funkce
 countOneTerminal:: [Char] -> [Char]
 countOneTerminal [] = error "Špatně uvedene Terminaly! Čarka na konci řádku?"
 countOneTerminal x = if (isStringLower x  && (length x == 1))
@@ -159,6 +285,4 @@ countOneTerminal x = if (isStringLower x  && (length x == 1))
              else error "Špatně uvedene Terminaly! Terminaly musi obsahovat pouze velke znaky!"
 
 
-
-fukce1 content = putStrLn("volal jsem 1") >> putStrLn(content) -- TODO
 fukce2 content = putStrLn("volal jsem 2") >> putStrLn(content) -- TODO
